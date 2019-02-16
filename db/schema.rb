79897335a4e3b77d01e7378ad376965668ee581c
fqllen <hatakeyamakouhei@hatakeyamakouheinoMacBook-puro.local>
2019-02-16 19:13:07 +0900

    Error solid
    マイグレーションエラー解決

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190213060524) do
+ActiveRecord::Schema.define(version: 20190216100750) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
@@ -19,8 +19,6 @@ ActiveRecord::Schema.define(version: 20190213060524) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
