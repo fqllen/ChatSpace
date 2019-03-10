295c17ec1bdb3498f1a9e7ac5b2bcff75b468037
fqllen <hatakeyamakouhei@hatakeyamakouheinoMacBook-puro.local>
2019-03-10 23:02:48 +0900

    インクリメンタルサーチの機能js、追加、削除のクリックイベント
    イベントの作成を非同期通信で動くように

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -17,7 +17,7 @@ $(function() {
       data: {keyword: input},
       dataType: 'json'
     })
-   .done(function(users) {
+    .done(function(users) {
       $("#user-search-result").empty();
       if (users.length !== 0) {
         users.forEach(function(user){
@@ -25,10 +25,32 @@ $(function() {
         });
       }
     })
-
     .fail(function(){
       alert('通信に失敗しました');
     });
+  });
 
+  function clickHTML(user){
+    var userId = user.attr("data-user-id");
+    var html = `
+                  <div class='chat-group-user clearfix js-chat-member' id='${userId}'>
+                     <input name='group[user_ids][]' type='hidden' value='${userId}'>
+                     <p class='chat-group-user__name'>${user.attr("data-user-name")}</p>
+                     <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                  </div>
+               `
+    return html;
+  };
+
+  $(document).on("click",".user-search-add", function() {
+    $input = $(this);
+    var add_user_html = clickHTML($input);
+    $("#search-users").append(add_user_html);
+      $input.parent().remove();
+  });
+
+  $(document).on("click",".user-search-remove", function() {
+    $input = $(this);
+    $input.parent().remove();
   });
 });
