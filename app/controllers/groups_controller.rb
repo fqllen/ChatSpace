c93682360d4ed1dd02784bac095bb3797f2e1cef
fqllen <hatakeyamakouhei@hatakeyamakouheinoMacBook-puro.local>
2019-03-10 20:27:36 +0900

    createにcurrent_user
    createアクションにcrrent_userを定義
    ビューで定義する為

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,11 +6,11 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
-    @group.users << current_user
   end
 
   def create
     @group = Group.new(group_params)
+    @group.users << current_user
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
@@ -33,7 +33,7 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-      params.require(:group).permit(:name, { user_ids: [] })
+    params.require(:group).permit(:name, { user_ids: [] })
   end
 
   def set_group
