cbb615ae1dc6e135c57a17bdd34d9d2c09e1f2d5
fqllen <kohei199875@icloud.com>
2019-03-13 17:04:39 +0900

    修正
    修正

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,54 +1,6 @@
 $(function() {
   function buildHTML(message){
-    if (message.image) {
-      addImage = `<img src="${message.image}" class="lower-message__image">`;
-    }
-    var html = `
-        <div class="message" data-message-id="${message.id}">
-          <div class="message-box">
-            <div class="message-box__person">${message.user_name}</div>
-            <div class="message-box__person--timestamp">${message.date}</div>
-          </div>
-          <div class="message-box__text">
-            <p class="message-box__text">
-              ${message.content}
-            </p>
-            ${addImage}
-          </div>
-        </div>`;
-    return html;
-  }
-  $('#new_message').on('submit',function(e) {
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr("action")
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(message){
-      $('.submit-btn').prop('disabled', false);
-    if(message.length != 0){
-      var html = buildHTML(message);
-      $('.messages').append(html)
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      $('#new_message')[0].reset();
-    }
-    })
-    .fail(function(){
-      alert('error');
-    })
-  })
-
-
-  function buildHTML(message){
-    if (message.image) {
-      addImage = `<img src="${message.image}" class="lower-message__image">`;
-    }
+    var addImage = message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`
     var html = `
                   <div class="message" data-message-id="${message.id}">
                     <div class="message-box">
@@ -65,13 +17,40 @@ $(function() {
                 `
     $('.messages').append(html);
   };
+
+  $('#new_message').on('submit',function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr("action")
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      $('.submit-btn').prop('disabled', false);
+    if(message.length != 0){
+      buildHTML(message);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('#new_message')[0].reset();
+    }
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+
+
+
   if (window.location.href.match(/\/groups\/\d+\/messages/)){
        setInterval(autoUpdate,5000)
   };
 
   function autoUpdate() {
     var href = $(this).attr("action")
-
       if($('.messages')[0]){
         var message_id = $('.message').last().attr('data-message-id');
         var group_id = $('.main-header__box__title').last().attr('data-group-id');
