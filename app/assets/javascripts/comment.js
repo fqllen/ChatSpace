8de6afb5b39944a1513ff5c067bf364ade214ba6
fqllen <kohei199875@icloud.com>
2019-03-13 16:14:59 +0900

    自動更新機能
    常に更新しないと、会話がをするたびに更新しなくてはいけないから

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,8 +1,8 @@
 $(function() {
   function buildHTML(message){
     var addImage = '';
-    if (message.image.url) {
-      addImage = `<img src="${message.image.url}" class="lower-message__image">`;
+    if (message.image) {
+      addImage = `<img src="${message.image}" class="lower-message__image">`;
     }
     var html = `
         <div class="message" data-message-id="${message.id}">
@@ -22,7 +22,7 @@ $(function() {
   $('#new_message').on('submit',function(e) {
     e.preventDefault();
     var formData = new FormData(this);
-    var url = window.location.href
+    var url = $(this).attr("action")
     $.ajax({
       url: url,
       type: "POST",
@@ -32,14 +32,73 @@ $(function() {
       contentType: false
     })
     .done(function(message){
+      $('.submit-btn').prop('disabled', false);
+    if(message.length != 0){
       var html = buildHTML(message);
       $('.messages').append(html)
-      $('#new_message')[0].reset();
-      $('.submit-btn').prop('disabled', false);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('#new_message')[0].reset();
+    }
     })
     .fail(function(){
       alert('error');
     })
   })
+
+
+  function addNewMessagesHTML(message){
+    var addImage = '';
+    if (message.image) {
+      addImage = `<img src="${message.image}" class="lower-message__image">`;
+    }
+    var html = `
+                  <div class="message" data-message-id="${message.id}">
+                    <div class="message-box">
+                      <div class="message-box__person">${message.user_name}</div>
+                      <div class="message-box__person--timestamp">${message.date}</div>
+                    </div>
+                    <div class="message-box__text">
+                      <p class="message-box__text">
+                        ${message.content}
+                      </p>
+                      ${addImage}
+                    </div>
+                  </div>
+                `
+    $('.messages').append(html);
+  };
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){
+       setInterval(autoUpdate,5000)
+  };
+
+  function autoUpdate() {
+    var href = $(this).attr("action")
+
+      if($('.messages')[0]){
+        var message_id = $('.message').last().attr('data-message-id');
+        var group_id = $('.main-header__box__title').last().attr('data-group-id');
+      } else { //ない場合は
+        var message_id = 0
+      }
+
+    $.ajax({
+      url: href,
+      type: 'GET',
+      dataType: 'json',
+      data: { message_id: message_id,
+        group_id: group_id
+      } //railsに引き渡すデータは
+          //このような形(paramsの形をしています)で、'id'には'message_id'を入れる
+    })
+
+    .done(function(messages) {
+      $.each(messages, function(i, message){ //'messages'を'message'に代入してeachで回す
+        addNewMessagesHTML(message);
+      });
+    })
+    .fail(function(){
+      alert('メッセージの取得に失敗しました');
+    });
+  };
 });
+
